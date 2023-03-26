4dddcb4b34959e31b74067c6511a86a55890dcd2
artcha026 <achavira@mscdirect.com.mx>
2023-03-26 17:58:28 -0600

    Fix made with

--- src/components/modules/Skills/index.tsx
+++ src/components/modules/Skills/index.tsx
@@ -15,8 +15,12 @@ const Skills = () => (
       <div className="flex-none lg:flex-1 pl-0 lg:pl-8">
         <h2 className="mb-8 text-3xl font-bold text-brand-primary dark:text-white typography">More about me</h2>
         <p className="mb-[2.5rem] font-normal text-xl md:text-[26px] text-brand-secondary dark:text-slate-200 leading-[34px] typography">
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s
-          standard dummy.
+				Software Developer with expertise in design, installation, testing
+				and maintenance of software systems. Equipped with a diverse and
+				promising skill-set. Proficient in various platforms, languages, and
+				embedded systems. Experienced with cutting-edge development tools
+				and procedures. Able to effectively self-manage during independent
+				projects, as well as collaborate as part of a productive team.
         </p>
         <Link href="#contact" className="button button-primary">
           Hire me
