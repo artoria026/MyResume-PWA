07dfdce36213fc0df5a21d4c335c88f1ea5ad2e5
artoria026 <artoria026@protonmail.com>
2023-03-27 01:27:48 -0600

    New script structure v3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,21 @@ import Contact from 'components/modules/Contact';
 import AllProviders from 'components/AllProviders';
 import Footer from 'components/ui/theme/Footer';
 import Scripts from 'components/Scripts';
+import * as serviceWorkerRegistration from '../serviceWorkerRegistration';
 
 const HomePage = () => (
-  <>
-    <Scripts />
-    <AllProviders>
-      <Intro />
-      {/* @ts-expect-error Server Component */}
-      <Projects />
-      <Skills />
-      <Contact />
-      <Footer />
-    </AllProviders>
-  </>
+	<>
+		<Scripts />
+		<AllProviders>
+			<Intro />
+			{/* @ts-expect-error Server Component */}
+			<Projects />
+			<Skills />
+			<Contact />
+			<Footer />
+		</AllProviders>
+	</>
 );
 
+serviceWorkerRegistration.unregister();
 export default HomePage;
--- src/components/Scripts/index.tsx
+++ src/components/Scripts/index.tsx
@@ -2,53 +2,28 @@
 
 import Script from 'next/script';
 import config from 'data/config';
-import { nanoid } from 'nanoid';
 
-const Scripts = () => {
-	const gtagId = `gtag-${nanoid()}`;
-	const swId = `sw-${nanoid()}`;
-
-	return (
-		<>
-			<Script
-				strategy="afterInteractive"
-				src={`https://www.googletagmanager.com/gtag/js?id=${config.googleAnalyticsID}`}
-				id={gtagId}
-			/>
-			<Script
-				strategy="afterInteractive"
-				dangerouslySetInnerHTML={{
-					__html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${config.googleAnalyticsID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-				}}
-				id={`gtag-config-${nanoid()}`}
-			/>
-			<Script
-				strategy="afterInteractive"
-				dangerouslySetInnerHTML={{
-					__html: `
-            if ('serviceWorker' in navigator) {
-              console.log("Puedes usar el service worker");
-
-              navigator.serviceWorker.register('./sw.js')
-                .then(res => console.log("SW cargado correctamente", res))
-                .catch(err => console.log("Service Worker No se ha podido registrar", err));
-
-            } else {
-              console.log("No se puede usar el service worker");
-            }
-          `,
-				}}
-				id={swId}
-			/>
-		</>
-	);
-};
+const Scripts = () => (
+	<>
+		<Script
+			strategy="afterInteractive"
+			src={`https://www.googletagmanager.com/gtag/js?id=${config.googleAnalyticsID}`}
+		/>
+		<Script
+			id="gtag"
+			strategy="afterInteractive"
+			dangerouslySetInnerHTML={{
+				__html: `
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						gtag('config', '${config.googleAnalyticsID}', {
+							page_path: window.location.pathname,
+						});
+					`,
+			}}
+		/>
+	</>
+);
 
 export default Scripts;
