d26416fdc4d68003e74217226b8e97d8235b586e
artoria026 <artoria026@protonmail.com>
2023-03-27 01:12:43 -0600

    New script structure

--- src/components/Scripts/index.tsx
+++ src/components/Scripts/index.tsx
@@ -25,7 +25,20 @@ const Scripts = () => (
 		/>
 		<Script
 			strategy="afterInteractive"
-			src="../../main.js"
+			dangerouslySetInnerHTML={{
+				__html: `
+          if ('serviceWorker' in navigator) {
+            console.log("Puedes usar el service worker");
+
+            navigator.serviceWorker.register('./sw.js')
+              .then(res => console.log("SW cargado correctamente", res))
+              .catch(err => console.log("Service Worker No se ha podido registrar", err));
+
+          } else {
+            console.log("No se puede usar el service worker");
+          }
+        `,
+			}}
 		/>
 	</>
 );
