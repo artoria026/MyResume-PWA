fcabf095874d07fdc7f29d4ed0d0e988e96f3327
artoria026 <artoria026@protonmail.com>
2023-03-27 01:02:55 -0600

    testing service worker

--- src/components/Scripts/index.tsx
+++ src/components/Scripts/index.tsx
@@ -4,26 +4,30 @@ import Script from 'next/script';
 import config from 'data/config';
 
 const Scripts = () => (
-  <>
-    <Script
-      strategy="afterInteractive"
-      src={`https://www.googletagmanager.com/gtag/js?id=${config.googleAnalyticsID}`}
-    />
-    <Script
-      id="gtag"
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `
-						window.dataLayer = window.dataLayer || [];
-						function gtag(){dataLayer.push(arguments);}
-						gtag('js', new Date());
-						gtag('config', '${config.googleAnalyticsID}', {
-							page_path: window.location.pathname,
-						});
-					`,
-      }}
-    />
-  </>
+	<>
+		<Script
+			strategy="afterInteractive"
+			src={`https://www.googletagmanager.com/gtag/js?id=${config.googleAnalyticsID}`}
+		/>
+		<Script
+			id="gtag"
+			strategy="afterInteractive"
+			dangerouslySetInnerHTML={{
+				__html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${config.googleAnalyticsID}', {
+            page_path: window.location.pathname,
+          });
+        `,
+			}}
+		/>
+		<Script
+			strategy="afterInteractive"
+			src="src/main.js"
+		/>
+	</>
 );
 
 export default Scripts;
