2ad9af8c4b9cbbcf04bfa0028c8ab8f02a29f321
artoria026 <artoria026@protonmail.com>
2023-03-27 01:16:33 -0600

    New script structure v2

--- src/components/Scripts/index.tsx
+++ src/components/Scripts/index.tsx
@@ -2,45 +2,53 @@
 
 import Script from 'next/script';
 import config from 'data/config';
+import { nanoid } from 'nanoid';
 
-const Scripts = () => (
-	<>
-		<Script
-			strategy="afterInteractive"
-			src={`https://www.googletagmanager.com/gtag/js?id=${config.googleAnalyticsID}`}
-		/>
-		<Script
-			id="gtag"
-			strategy="afterInteractive"
-			dangerouslySetInnerHTML={{
-				__html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${config.googleAnalyticsID}', {
-            page_path: window.location.pathname,
-          });
-        `,
-			}}
-		/>
-		<Script
-			strategy="afterInteractive"
-			dangerouslySetInnerHTML={{
-				__html: `
-          if ('serviceWorker' in navigator) {
-            console.log("Puedes usar el service worker");
+const Scripts = () => {
+	const gtagId = `gtag-${nanoid()}`;
+	const swId = `sw-${nanoid()}`;
 
-            navigator.serviceWorker.register('./sw.js')
-              .then(res => console.log("SW cargado correctamente", res))
-              .catch(err => console.log("Service Worker No se ha podido registrar", err));
+	return (
+		<>
+			<Script
+				strategy="afterInteractive"
+				src={`https://www.googletagmanager.com/gtag/js?id=${config.googleAnalyticsID}`}
+				id={gtagId}
+			/>
+			<Script
+				strategy="afterInteractive"
+				dangerouslySetInnerHTML={{
+					__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${config.googleAnalyticsID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+				}}
+				id={`gtag-config-${nanoid()}`}
+			/>
+			<Script
+				strategy="afterInteractive"
+				dangerouslySetInnerHTML={{
+					__html: `
+            if ('serviceWorker' in navigator) {
+              console.log("Puedes usar el service worker");
 
-          } else {
-            console.log("No se puede usar el service worker");
-          }
-        `,
-			}}
-		/>
-	</>
-);
+              navigator.serviceWorker.register('./sw.js')
+                .then(res => console.log("SW cargado correctamente", res))
+                .catch(err => console.log("Service Worker No se ha podido registrar", err));
+
+            } else {
+              console.log("No se puede usar el service worker");
+            }
+          `,
+				}}
+				id={swId}
+			/>
+		</>
+	);
+};
 
 export default Scripts;
